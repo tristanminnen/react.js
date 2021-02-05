189527b18b4e544f28e0a0eb5cdcd4bbfa83d74c
tristanminnen <47850370+tristanminnen@users.noreply.github.com>
2021-02-05 16:32:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+        producten: [
+            {
+                product: 'banaan' ,
+                prijs: 2,
+                aantal:0,
+                voorraad:12,
+                img:"img/banaan.png",
+            },
+            {
+                product: 'appel' ,
+                prijs: 1,
+                aantal:0,
+                voorraad:8,
+                img:"img/appel.png",
+
+            },
+            {
+                product: 'peer' ,
+                prijs: 3,
+                aantal:0,
+                voorraad:9,
+                img:"img/peer.png",
+
+            },
+            {
+                product: 'Kiwi' ,
+                prijs: 4,
+                aantal:0,
+                voorraad:8,
+                img:"img/kiwi.png",
+
+            },
+            {
+                product: 'Bloemkool' ,
+                prijs: 2,
+                aantal:0,
+                voorraad:8,
+                img:"img/bloemkool.png",
+
+            },
+            {
+                product: 'broccoli' ,
+                prijs: 3,
+                aantal:0,
+                voorraad:8,
+                img:"img/broccoli.png",
+
+            },
+        ],
+        totaal:0,
+        totaantal:0
+    }
+})
+
+
+
